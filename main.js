05ffcae2bc5d4d5a45511326ea7ce15888f9843f
truong.nguyen <truong.xuan@bmbsoft.com.vn>
2020-08-18 14:06:42 +0700

    Add dont

--- main.js
+++ main.js
@@ -221,4 +221,273 @@ function init() {
             });
         }
     });
+
+    $("#type").change(function () {
+        var type = $('#type').val();
+        map.getInteractions().array_.forEach(function (interaction) {
+            if (interaction instanceof ol.interaction.Draw) {
+                map.removeInteraction(interaction);
+                return;
+            }
+        })
+
+        function calculateMeasure() {
+            var sketch;
+            var helpTooltipElement;
+            var helpTooltip;
+            var measureTooltipElement;
+            var measureTooltip;
+            var continuePolygonMsg = 'Click to continue drawing the polygon';
+            var continueLineMsg = 'Click to continue drawing the line';
+
+            var pointerMoveHandler = function (evt) {
+                if (evt.dragging) {
+                    return;
+                }
+                /** @type {string} */
+                var helpMsg = 'Click to start drawing';
+
+                if (sketch) {
+                    var geom = sketch.getGeometry();
+                    if (geom instanceof ol.geom.Polygon) {
+                        helpMsg = continuePolygonMsg;
+                    } else if (geom instanceof ol.geom.LineString) {
+                        helpMsg = continueLineMsg;
+                    }
+                }
+
+                helpTooltipElement.innerHTML = helpMsg;
+                helpTooltip.setPosition(evt.coordinate);
+
+                helpTooltipElement.classList.remove('hidden');
+            };
+
+            map.on('pointermove', pointerMoveHandler);
+            map.getViewport().addEventListener('mouseout', function () {
+                helpTooltipElement.classList.add('hidden');
+            });
+
+            var typeSelect = document.getElementById('type');
+
+            var draw; // global so we can remove it later
+
+            /**
+             * Format length output.
+             * @param {LineString} line The line.
+             * @return {string} The formatted length.
+             */
+            var formatLength = function (line) {
+                var length = ol.sphere.getLength(line);
+                var output;
+                if (length > 100) {
+                    output = Math.round((length / 1000) * 100) / 100 + ' ' + 'km';
+                } else {
+                    output = Math.round(length * 100) / 100 + ' ' + 'm';
+                }
+                return output;
+            };
+
+            /**
+             * Format area output.
+             * @param {Polygon} polygon The polygon.
+             * @return {string} Formatted area.
+             */
+            var formatArea = function (polygon) {
+                var area = ol.sphere.getArea(polygon);
+                var output;
+                if (area > 10000) {
+                    output = Math.round((area / 1000000) * 100) / 100 + ' ' + 'km<sup>2</sup>';
+                } else {
+                    output = Math.round(area * 100) / 100 + ' ' + 'm<sup>2</sup>';
+                }
+                return output;
+            };
+
+            function addInteraction() {
+                var type = typeSelect.value === 'area' ? 'Polygon' : 'LineString';
+                draw = new ol.interaction.Draw({
+                    source: map,
+                    type: type,
+                    style: new ol.style.Style({
+                        fill: new ol.style.Fill({
+                            color: 'rgba(255, 255, 255, 0.2)',
+                        }),
+                        stroke: new ol.style.Stroke({
+                            color: 'rgba(0, 0, 0, 0.5)',
+                            lineDash: [10, 10],
+                            width: 2,
+                        }),
+                        image: new ol.style.Circle({
+                            radius: 5,
+                            stroke: new ol.style.Stroke({
+                                color: 'rgba(0, 0, 0, 0.7)',
+                            }),
+                            fill: new ol.style.Fill({
+                                color: 'rgba(255, 255, 255, 0.2)',
+                            }),
+                        }),
+                    }),
+                });
+                map.addInteraction(draw);
+
+                createMeasureTooltip();
+                createHelpTooltip();
+
+                var listener;
+                draw.on('drawstart', function (evt) {
+                    // set sketch
+                    sketch = evt.feature;
+
+                    /** @type {import("../src/ol/coordinate.js").Coordinate|undefined} */
+                    var tooltipCoord = evt.coordinate;
+
+                    listener = sketch.getGeometry().on('change', function (evt) {
+                        var geom = evt.target;
+                        var output;
+                        if (geom instanceof ol.geom.Polygon) {
+                            output = formatArea(geom);
+                            tooltipCoord = geom.getInteriorPoint().getCoordinates();
+                        } else if (geom instanceof ol.geom.LineString) {
+                            output = formatLength(geom);
+                            tooltipCoord = geom.getLastCoordinate();
+                        }
+                        measureTooltipElement.innerHTML = output;
+                        measureTooltip.setPosition(tooltipCoord);
+                    });
+                });
+
+                draw.on('drawend', function () {
+                    measureTooltipElement.className = 'ol-tooltip ol-tooltip-static';
+                    measureTooltip.setOffset([0, -7]);
+                    // unset sketch
+                    sketch = null;
+                    // unset tooltip so that a new one can be created
+                    measureTooltipElement = null;
+                    createMeasureTooltip();
+                    ol.Observable.unByKey(listener);
+                });
+            }
+
+            /**
+             * Creates a new help tooltip
+             */
+            function createHelpTooltip() {
+                if (helpTooltipElement) {
+                    helpTooltipElement.parentNode.removeChild(helpTooltipElement);
+                }
+                helpTooltipElement = document.createElement('div');
+                helpTooltipElement.className = 'ol-tooltip hidden';
+                helpTooltip = new ol.Overlay({
+                    element: helpTooltipElement,
+                    offset: [15, 0],
+                    positioning: 'center-left',
+                });
+                map.addOverlay(helpTooltip);
+            }
+
+            /**
+             * Creates a new measure tooltip
+             */
+            function createMeasureTooltip() {
+                if (measureTooltipElement) {
+                    measureTooltipElement.parentNode.removeChild(measureTooltipElement);
+                }
+                measureTooltipElement = document.createElement('div');
+                measureTooltipElement.className = 'ol-tooltip ol-tooltip-measure';
+                measureTooltip = new ol.Overlay({
+                    element: measureTooltipElement,
+                    offset: [0, -15],
+                    positioning: 'bottom-center',
+                });
+                map.addOverlay(measureTooltip);
+            }
+
+            /**
+             * Let user change the geometry type.
+             */
+            typeSelect.onchange = function () {
+                map.removeInteraction(draw);
+                addInteraction();
+            };
+
+            addInteraction();
+        }
+
+        function zoomIn() {
+            map.getView().setZoom(map.getView().getZoom() + 1);
+        }
+
+        function zoomOut() {
+            map.getView().setZoom(map.getView().getZoom() - 1)
+        }
+
+        function exportMap() {
+            map.once('postcompose', function (event) {
+                var canvas = event.context.canvas;
+                var data = canvas.toDataURL("image/png");
+
+                if (navigator.msSaveBlob) {
+                    navigator.msSaveBlob(canvas.msToBlob(), 'map.png');
+                } else {
+                    canvas.toBlob(function (blob) {
+                        saveAs(blob, 'map.png');
+                    });
+                }
+
+            });
+            map.renderSync();
+        }
+
+        function selectOne() {
+            this.ctr_selectFeature = new ol.interaction.Select({
+                condition: ol.events.condition.click
+            });
+            map.addInteraction(this.ctr_selectFeature);
+            this.ctr_selectFeature.on('select', function (e) {
+                var n = e.target.getFeatures().getLength();
+                if (n == 0) return;
+            });
+        }
+
+        switch (type) {
+            case "icon-fullScreen":
+                break;
+            case "icon-selectOne":
+                selectOne();
+                break;
+            case "icon-selectMany":
+                break;
+            case "icon-pan":
+                break;
+            case "icon-pin":
+                break;
+            case "icon-location":
+                break;
+            case "icon-zoomIn":
+                zoomIn();
+                break;
+            case "icon-zoomOut":
+                zoomOut();
+                break;
+            case "LineString":
+                calculateMeasure();
+                break;
+            case "area":
+                calculateMeasure();
+                break;
+            case "icon-exportmap":
+                exportMap();
+                break;
+            case "icon-print":
+                break;
+            case "icon-filter":
+                break;
+            case "icon-legend":
+                break;
+            case "icon-identify":
+                break;
+            case "icon-duan":
+                break;
+        }
+    })
 }
